224503d240ed25fa787514566678e18bcadfa138
artemgey <98397878+artemgey@users.noreply.github.com>
2023-03-10 18:09:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let input = document.querySelector('#display');
+let key = document.querySelector('.key')
+let keyboard = document.querySelectorAll('.key')
+let exp = '';
+for (let key of keyboard) {
+    key.addEventListener('click', function () {
+        exp = exp + key.textContent;
+        input.value = exp;
+        console.log(input);
+    })
+}
